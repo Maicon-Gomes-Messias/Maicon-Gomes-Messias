7ab7291b820741a305ddb2c2cd34c55659f8eba4
Maicon-Gomes-Messias <67137555+Maicon-Gomes-Messias@users.noreply.github.com>
2022-03-16 16:18:20 -0300

    Delete comentario/comentario directory

--- comentario/comentario/src/Registro.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public interface Registro {
-    public int getID();
-    public void setID(int ID);
-    public byte[] toByteArray() throws IOException;
-    public void fromByteArray(byte[] ba) throws IOException;
-}
